3521a8e87cb4fe469f076f3aa9d2d7a26e7af992
anasouza0087 <anasouza0087@users.noreply.github.com>
2021-02-10 21:45:35 -0300

    Finalização do projeto

--- scripts.js
+++ scripts.js
@@ -7,32 +7,33 @@ clickBtn.addEventListener("click", e => {
     let calcIMC = (weight / (size * size)).toFixed(2)
     document.getElementById("imc").value = calcIMC
 
-    switch (calcIMC) {
-        case (calcIMC < 16.00):
-            categoria = document.getElementById("categoria").value = "Baixo peso Grau III"
-            break;
-        case (calcIMC >= 16.00 && calcIMC <= 16.99):
-            categoria = document.getElementById("categoria").value = "Baixo peso Grau II"
-            break;
-        case (calcIMC >= 17.00 && calcIMC <= 18.49):
-            categoria = document.getElementById("categoria").value = "Baixo peso Grau I"
-            break;
-        case (calcIMC >= 18.50 && calcIMC <= 24.99):
-            categoria = document.getElementById("categoria").value = "Peso ideal"
-            break;
-        case (calcIMC >= 25.00 && calcIMC <= 29.99):
-            categoria = document.getElementById("categoria").value = "Sobrepeso"
-            break;
-        case (calcIMC >= 30.00 && calcIMC <= 34.99):
-            categoria = document.getElementById("categoria").value = "Obesidade Grau I"
-            break;
-        case (calcIMC >= 35.00 && calcIMC <= 39.99):
-            categoria = document.getElementById("categoria").value = "Obesidade Grau II"
-            break;
-        case (calcIMC >= 40.00):
-            categoria = document.getElementById("categoria").value = "Obesidade Grau III"
-            break;
-        default:
-            categoria = document.getElementById("categoria").value = "Inválido"
+    if (calcIMC < 16.00) {
+        categoria = document.getElementById("categoria").value = "Baixo peso Grau III"
+        categoria = document.getElementById("categoria").style.backgroundColor = "red"
+    } else if (calcIMC >= 16.00 && calcIMC <= 16.99) {
+        categoria = document.getElementById("categoria").value = "Baixo peso Grau II"
+        categoria = document.getElementById("categoria").style.backgroundColor = "orange"
+    } else if (calcIMC >= 17.00 && calcIMC <= 18.49) {
+        categoria = document.getElementById("categoria").value = "Baixo peso Grau I"
+        categoria = document.getElementById("categoria").style.backgroundColor = "yellow"
+    } else if (calcIMC >= 18.50 && calcIMC <= 24.99) {
+        categoria = document.getElementById("categoria").value = "Peso ideal"
+        categoria = document.getElementById("categoria").style.backgroundColor = "lightblue"
+    } else if (calcIMC >= 25.00 && calcIMC <= 29.99) {
+        categoria = document.getElementById("categoria").value = "Sobrepeso"
+        categoria = document.getElementById("categoria").style.backgroundColor = "yellow"
+    } else if (calcIMC >= 30.00 && calcIMC <= 34.99) {
+        categoria = document.getElementById("categoria").value = "Obesidade Grau I"
+        categoria = document.getElementById("categoria").style.backgroundColor = "orange"
+    } else if (calcIMC >= 35.00 && calcIMC <= 39.99) {
+        categoria = document.getElementById("categoria").value = "Obesidade Grau II"
+        categoria = document.getElementById("categoria").style.backgroundColor = "orange"
+    } else if (calcIMC >= 40.00) {
+        categoria = document.getElementById("categoria").value = "Obesidade Grau III"
+        categoria = document.getElementById("categoria").style.backgroundColor = "red"
+    } else {
+        categoria = document.getElementById("categoria").value = "Valor inválido"
+        categoria = document.getElementById("categoria").style.backgroundColor = "red"
     }
+
 })
